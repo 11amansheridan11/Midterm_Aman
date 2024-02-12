787de3b80493f9954193b4d04c54c308d3c01649
11amansheridan11 <159819045+11amansheridan11@users.noreply.github.com>
2024-02-12 13:38:35 -0500

    Update Arithmetic.java

--- src/arithmetic/Arithmetic.java
+++ src/arithmetic/Arithmetic.java
@@ -1,36 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package arithmetic;
 
-
 import java.util.Scanner;
-import static java.time.Clock.system;
+
+// Enum for arithmetic operations
+enum Operation {
+    PLUS,
+    MINUS,
+    TIMES,
+    DIVIDE
+}
 
 /** This class calls the method to perform 
  * arithmetic operations based on user input
  * execute the code check the output
  * @author sivagamasrinivasan
- * 
  */
-public class Arithmetic 
-{
+public class Arithmetic {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) 
-    {
-       
-        ArithmeticBase r= new ArithmeticBase();
-        Scanner in= new Scanner(System.in);
-        int n= in.nextInt();
-        int m= in.nextInt();
-        double result = r.calculate(m,n);
-        System.out.println("result :" +result); 
-    
+    public static void main(String[] args) {
+
+        ArithmeticBase r = new ArithmeticBase();
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter two numbers: ");
+        double n = in.nextDouble();
+        double m = in.nextDouble();
+
+        System.out.println("Enter arithmetic operation to Perform (PLUS, MINUS, TIMES, DIVIDE): ");
+        Operation operation = Operation.valueOf(in.next().toUpperCase());
+
+        double result = r.calculate(n, m, operation);
+        System.out.println("result: " + result);
     }
 }
-
